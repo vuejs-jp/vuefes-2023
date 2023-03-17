bec7475ba08858c9efdbfe3cedf8b01f78ac6508
toshick <toshickjazz@mac.com>
2023-03-17 23:24:00 +0900

    fix: ogDescription

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,7 +3,7 @@ export const navLinks = [{ text: 'Contact', link: '/#contact' }]
 export const conferenceTitle = 'Vue Fes Japan Online 2023'
 
 export const ogDescription =
-  '（ダミー文言）2023年10月16日（日）に開催される日本最大級の Vue.js オンラインカンファレンス'
+  '2023年10月28日（土）に開催される日本最大級の Vue.js オンラインカンファレンス。国内外の著名スピーカーによるセッションの他、初心者向けハンズオンなどのイベントも企画しています。ぜひ、一緒に Vue.js を楽しみ、盛り上げていきましょう！'
 
 export const linkUrl = 'https://vuefes.jp/2023/'
 

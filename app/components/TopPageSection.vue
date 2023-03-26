4342a42f40af816884788fad88ba1a9764c39de6
watsuyo <watsuyo.dev@gmail.com>
2023-03-27 00:41:14 +0900

    Implemented the top page

--- app/components/TopPageSection.vue
+++ app/components/TopPageSection.vue
@@ -1,3 +1,137 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted, computed } from 'vue'
+import PcBlock from '@/assets/top/pc/block.svg'
+import PcTitle from '@/assets/top/pc/title.svg'
+import PcWave from '@/assets/top/pc/wave.svg'
+import MdBlock from '@/assets/top/md/block.svg'
+import MdTitle from '@/assets/top/md/title.svg'
+import MdBg from '@/assets/top/md/wave.svg'
+import SpBlock from '@/assets/top/sp/block.svg'
+import SpTitle from '@/assets/top/sp/title.svg'
+import SpBg from '@/assets/top/sp/wave.svg'
+
+type Device = 'pc' | 'md' | 'sp'
+type ImageList = {
+  wave: typeof PcWave | typeof MdBg | typeof SpBg
+  block: typeof PcBlock | typeof MdBlock | typeof SpBlock
+  title: typeof PcTitle | typeof MdTitle | typeof SpTitle
+}
+
+const device = ref<Device>('pc')
+
+const getImageListByDevice = (device: Device): ImageList => {
+  const imageListMap: Record<Device, ImageList> = {
+    pc: {
+      wave: PcWave,
+      block: PcBlock,
+      title: PcTitle,
+    },
+    md: {
+      wave: MdBg,
+      block: MdBlock,
+      title: MdTitle,
+    },
+    sp: {
+      wave: SpBg,
+      block: SpBlock,
+      title: SpTitle,
+    },
+  }
+
+  return imageListMap[device]
+}
+
+const updateDevice = () => {
+  if (process.client && window.innerWidth >= 1024) {
+    device.value = 'pc'
+  } else if (process.client && window.innerWidth >= 768) {
+    device.value = 'md'
+  } else {
+    device.value = 'sp'
+  }
+}
+
+const handleResize = () => {
+  updateDevice()
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+  handleResize()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+})
+
+const components = computed<ImageList>(() => getImageListByDevice(device.value))
+</script>
+
 <template>
-  <section>Top</section>
+  <div class="top-content">
+    <div class="components-wrapper">
+      <component :is="components.wave" class="bg" />
+      <component :is="components.block" class="components" />
+      <div class="title-wrapper">
+        <component :is="components.title" class="title" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="ts" scoped>
+css({
+  '.top-content': {
+    position: 'relative',
+    backgroundColor: '#35495E',
+  },
+  '.components': {
+    width: '100%',
+  },
+  '.components-wrapper': {
+    display: 'inline-block',
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+  },
+  '.bg': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 2,
+  },
+  '.title-wrapper': {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  '.title': {
+    width: '80%',
+    height: 'auto',
+  },
+  '@media(min-width: 1024px)': {
+    '.title-wrapper': {
+      left: '45%',
+    }
+  },
+  '@media(min-width: 768px) and (max-width: 1023px)': {
+    '.title': {
+      width: '90vw',
+    }
+  },
+  '@media(max-width: 767px)': {
+    '.title': {
+      width: '90vw',
+    },
+    '.components-wrapper': {
+      height: '70vh',
+    },
+    '.components': {
+      height: '100%',
+    },
+  },
+})
+</style>

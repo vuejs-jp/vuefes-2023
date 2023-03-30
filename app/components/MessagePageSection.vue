50e25b264000cfaf86803838ccb2b306a8434224
maashi <octtn8c@gmail.com>
2023-03-30 13:05:36 +0900

    Add Message Section

--- app/components/MessagePageSection.vue
+++ app/components/MessagePageSection.vue
@@ -1,47 +1,122 @@
+<script setup lang="ts"></script>
+
 <template>
-  <section>
-    <div class="message-root">
-      <div class="title">
-        <h2 id="message">Message</h2>
-        <h3>想い</h3>
-      </div>
-      <div class="subtitle">
+  <section class="message">
+    <div class="message-body">
+      <h2 id="message" class="title">
+        <span class="mainTitle">
+          Message
+        </span>
+        <br>
+        <span class="subTitle">
+          想い
+        </span>
+      </h2>
+      <div class="text">
         <p>
-          Vue Fes Japan は 2018 年に誕生した日本最大級の Vue.js カンファレンスです。文字通り Fes =
-          お祭りのように Vue.js
-          を共に盛り上げ、共に学び、そしてなによりも共に楽しむために誕生しました。
+          Vue Fes Japan は 2018 年に誕生した日本最大級の Vue.js カンファレンスです。
+          <br>
+          文字通り Fes = お祭りのように Vue.js を共に盛り上げ、共に学び、そしてなによりも共に楽しむために誕生しました。
         </p>
         <p>
-          昨年 2022 年は長引くコロナ禍においてオンラインでの開催を実現し、650
-          名以上の事前登録、10,000 回以上の再生回数を記録し、大変盛り上がりました。特に、Vue 2 から
-          Vue 3 への移行に関する内容が多かったことが印象的でした。
+          昨年 2022 年は長引くコロナ禍においてオンラインでの開催を実現し、650 名以上の事前登録、10,000 回以上の再生回数を記録し、大変盛り上がりました。特に、Vue 2 から Vue 3 への移行に関する内容が多かったことが印象的でした。
         </p>
         <p>
-          2022 年 1 月、Vue.js は最新バージョンである Vue 3 をデフォルトにしました。その結果、npm
-          でのダウンロード数が 200% 以上増加しており、Vue 3
-          の利用が加速しています。エコシステムにおいても、2022 年 11 月に Vue 3 へ対応した Nuxt 3 や
-          Vuetify 3 がリリースされました。
+          2022 年 1 月、Vue.js は最新バージョンである Vue 3 をデフォルトにしました。その結果、npm でのダウンロード数が 200% 以上増加しており、Vue 3 の利用が加速しています。エコシステムにおいても、2022 年 11 月に Vue 3 へ対応した Nuxt 3 や Vuetify 3 がリリースされました。
         </p>
         <p>
-          こうして Vue 3 の利用が本格化する中、Vue.js の作者 Evan You 氏は次の Vue.js
-          に向けた取り組みも開始しています。同氏の開発した Vite が Vue.js
-          コミュニティだけに留まらず、他の JavaScript コミュニティへも放たれたのと同様に、Nuxt や
-          Volar といったエコシステムにおいても新しい展開を見せようとしています。
+          こうして Vue 3 の利用が本格化する中、Vue.js の作者 Evan You 氏は次の Vue.js に向けた取り組みも開始しています。同氏の開発した Vite が Vue.js コミュニティだけに留まらず、他の JavaScript コミュニティへも放たれたのと同様に、Nuxt や Volar といったエコシステムにおいても新しい展開を見せようとしています。
         </p>
         <p>
-          Vue.js とエコシステムがさらに発展しつつある中、この度 2023
-          年はオフラインカンファレンスとしての Vue Fes Japan をリブートすることを決意しました。
+          Vue.js とエコシステムがさらに発展しつつある中、この度 2023 年はオフラインカンファレンスとしての Vue Fes Japan をリブートすることを決意しました。
+          <br>
           ぜひ、一緒にカンファレンスに参加し、リアルでしか味わえない体験を楽しみましょう！
         </p>
       </div>
       <div class="representative">
-        Vue.js 日本ユーザーグループ代表 川口 和也（<a
-          href="https://twitter.com/kazu_pon"
-          target="_blank"
-          rel="noreferrer"
-          >@kazu_pon</a
-        >）
+        <p>
+          <span>Vue.js</span>
+          <span>日本ユーザーグループ</span>
+          <span>代表</span>
+          <span>川口 和也</span>
+          <span>(
+            <a href="https://twitter.com/kazu_pon" target="_blank" rel="noreferrer">
+              @kazu_pon
+            </a>
+            )</span>
+        </p>
       </div>
     </div>
   </section>
 </template>
+
+<style lang="ts" scoped>
+css({
+  '.message': {
+    padding: '40px 0 120px',
+    display: 'grid',
+    gridTemplateColumns: '1fr min(988px, calc(100% - 40px)) 1fr',
+    background: '{color.vue.blue}',
+  },
+  '.message-body': {
+    padding: '68px 11.53% 72px',
+    gridColumn: '2',
+    color: '{color.white}',
+    background: 'rgba(255, 255, 255, 0.05)',
+    borderRadius: '60px',
+  },
+  '.title': {
+    textAlign: 'center',
+    lineHeight: '1',
+  },
+  '.mainTitle': {
+    fontSize: 'calc(40*{fontSize.base})',
+    fontWeight: 'bold',
+    lineHeight: '1',
+  },
+  '.subTitle': {
+    textAlign: 'center',
+    fontSize: 'calc(20*{fontSize.base})',
+    lineHeight: '1',
+    marginTop: '18px',
+    display: 'grid',
+    gridTemplateColumns: '96px max-content 96px',
+    justifyContent: 'center',
+    placeItems: 'center',
+    columnGap: '16px',
+    
+    '&::before,&::after': {
+      content: '""',
+      display: 'inline-block',
+      background: '{color.white}',
+      width: '96px',
+      height: '1px',
+    },
+  },
+  '.text': {
+    marginTop: '32px',
+
+    'p': {
+      fontSize: 'calc(18*{fontSize.base})',
+      lineHeight: '1.8',
+      '& + &': {
+        marginTop: '1.8em',
+      },
+    }
+  },
+  '.representative': {
+    marginTop: '24px',
+    textAlign: 'right',
+    fontSize: 'calc(18*{fontSize.base})',
+    lineHeight: '1.8',
+
+    'span': {
+      display: 'inline-block',
+    },
+    
+    'span + span': {
+      marginLeft: '0.5em',
+    }
+  },
+})
+</style>
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,10 +1,23 @@
 import { defineTheme } from 'pinceau'
 
 // https://pinceau.dev/configuration/tokens-config
-defineTheme({
+export default defineTheme({
   media: {
     mobile: '(min-width: 320px)',
     tablet: '(min-width: 768px)',
     desktop: '(min-width: 1280px)',
+  },
+  color: {
+    'white': '#FFF',
+    'vue': {
+      'blue': '#35495E',
+      'green': '#42B983',
+    },
+    'typescript': {
+      'blue': '#3178C6',
+    },
+  },
+  fontSize: {
+    'base': '0.0625rem',
   },
 })

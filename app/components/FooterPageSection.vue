f5a5f109d36d96662a326d7ddd8df57c419f1fa6
nakamoto_yuto <yutonakamoto03@gmail.com>
2023-03-30 19:33:12 +0900

    add footer section

--- app/components/FooterPageSection.vue
+++ app/components/FooterPageSection.vue
@@ -1,3 +1,28 @@
+<script lang="ts" setup>
+import CopyrightText from '~/components/footer/CopyrightText.vue'
+import VueFesLogo from '~/assets/logo/vuefes_logo.svg'
+</script>
+
 <template>
-  <footer>Footer</footer>
+  <section class="footer-section">
+    <VueFesLogo class="footer-vuefes-logo" />
+    <CopyrightText />
+  </section>
 </template>
+
+<style lang="ts" scoped>
+css({
+  '.footer-section': {
+    position: 'relative',
+    background: '{color.vue-blue}',
+    padding: '80px 32px',
+    color: '{color.white}',
+    textAlign: 'center',
+    backgroundImage: 'url("~/assets/footer/background.svg")',
+    backgroundPosition: '50% 100%',
+  },
+  '.footer-vuefes-logo': {
+    margin: '0 auto 32px'
+  }
+})
+</style>

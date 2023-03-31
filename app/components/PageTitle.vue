e9e1afa71a9866f11180aab27551c2401e7f3490
jiyuujin <jiyuujinunite@gmail.com>
2023-03-31 10:29:25 +0900

    Code of Conduct - #82

---
+++ app/components/PageTitle.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  yamatoTitle: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <h1 :id="id" class="title">
+    <span class="title-main">{{ title }}</span>
+    <br />
+    <span class="title-sub">{{ yamatoTitle }}</span>
+  </h1>
+</template>
+
+<style lang="ts" scoped>
+css({
+  '.title': {
+    textAlign: 'center',
+    lineHeight: '1',
+  },
+  '.title-main': {
+    color: '{color.vue.blue}',
+    fontSize: 'calc(40*{fontSize.base})',
+    fontWeight: 'bold',
+    lineHeight: '1',
+  },
+  '.title-sub': {
+    textAlign: 'center',
+    color: '{color.vue.blue}',
+    fontSize: 'calc(20*{fontSize.base})',
+    lineHeight: '1',
+    marginTop: '18px',
+    display: 'grid',
+    gridTemplateColumns: '96px max-content 96px',
+    justifyContent: 'center',
+    placeItems: 'center',
+    columnGap: '16px',
+    '&::before, &::after': {
+      content: '""',
+      display: 'inline-block',
+      background: '{color.vue.blue}',
+      width: '96px',
+      height: '1px',
+    },
+  },
+})
+</style>
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -5,6 +5,9 @@ export const conferenceTitle = 'Vue Fes Japan 2023'
 export const ogDescription =
   '2023年10月28日（土）に開催される日本最大級の Vue.js カンファレンスです。国内外の著名スピーカーによるセッションの他、LT、初心者向けハンズオン、グッズ販売などのイベントも企画しています。ぜひ一緒に Vue.js を楽しみ、盛り上げていきましょう！'
 
+export const ogCoCDescription =
+  'Vue Fes Japan 2023 のすべての参加者、スピーカー、スポンサー、スタッフは、オープンかつ友好的な環境を育むため、以下の行動規範に同意していただく必要があります。'
+
 export const linkUrl = 'https://vuefes.jp/2023/'
 
 export const vuefesTwitterID = '@vuefes'

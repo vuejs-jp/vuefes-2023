703c05b7804e59dfad68f0a4536f67bf30f5cda7
jiyuujin <jiyuujinunite@gmail.com>
2023-03-31 10:35:52 +0900

    ignore privacy

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     'vue/multi-word-component-names': [
       'error',
       {
-        ignores: ['index', 'default', 'error'],
+        ignores: ['index', 'default', 'error', 'privacy'],
       },
     ],
     'vue/multiline-html-element-content-newline': 'off',

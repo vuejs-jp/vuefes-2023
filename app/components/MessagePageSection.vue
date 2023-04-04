a48c40fc211b391af89f8dec639091e5b8865c9c
maashi <octtn8c@gmail.com>
2023-04-04 16:47:45 +0900

    fix: lint

--- app/components/MessagePageSection.vue
+++ app/components/MessagePageSection.vue
@@ -12,14 +12,14 @@
           </p>
           <p>
             昨年 2022 年は長引くコロナ禍においてオンラインでの開催を実現し、650
-            名以上の事前登録、10,000 回以上の再生回数を記録し、大変盛り上がりました。特に、Vue 2 から
-            Vue 3 への移行に関する内容が多かったことが印象的でした。
+            名以上の事前登録、10,000 回以上の再生回数を記録し、大変盛り上がりました。特に、Vue 2
+            から Vue 3 への移行に関する内容が多かったことが印象的でした。
           </p>
           <p>
             2022 年 1 月、Vue.js は最新バージョンである Vue 3 をデフォルトにしました。その結果、npm
             でのダウンロード数が 200% 以上増加しており、Vue 3
-            の利用が加速しています。エコシステムにおいても、2022 年 11 月に Vue 3 へ対応した Nuxt 3 や
-            Vuetify 3 がリリースされました。
+            の利用が加速しています。エコシステムにおいても、2022 年 11 月に Vue 3 へ対応した Nuxt 3
+            や Vuetify 3 がリリースされました。
           </p>
           <p>
             こうして Vue 3 の利用が本格化する中、Vue.js の作者 Evan You 氏は次の Vue.js
@@ -42,7 +42,9 @@
             <span>川口 和也</span>
             <span
               >(
-              <a href="https://twitter.com/kazu_pon" target="_blank" rel="noreferrer"> @kazu_pon </a>
+              <a href="https://twitter.com/kazu_pon" target="_blank" rel="noreferrer">
+                @kazu_pon
+              </a>
               )</span
             >
           </p>

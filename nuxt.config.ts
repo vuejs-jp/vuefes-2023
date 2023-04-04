06dd828b2d37369dec9b6dcc54b9b34c380e0abe
jiyuujin <jiyuujinunite@gmail.com>
2023-04-04 22:53:21 +0900

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       htmlAttrs: {
         lang: 'ja',
       },

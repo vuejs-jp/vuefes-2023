df66cb47fac4f9dae4ba44bfa5d318bc97265a91
toshick <toshickjazz@mac.com>
2023-05-29 00:30:41 +0900

    feat: refactored to use RoundButton for all rounded buttons

--- app/components/forms/SubmitButton.vue
+++ app/components/forms/SubmitButton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ButtonHTMLAttributes } from 'vue'
+import RoundButton from '@/components/button/RoundButton.vue'
 
 type _SubmitButtonProps = Omit<ButtonHTMLAttributes, 'disabled' | 'onClick'>
 interface SubmitButtonProps extends _SubmitButtonProps {
@@ -21,30 +22,15 @@ const handleClick = () => {
 </script>
 
 <template>
-  <button type="submit" class="form-button" :disabled="disabled" @click="handleClick">
-    {{ titleLabel }}
-  </button>
+  <RoundButton type="submit" class="form-button" :disabled="disabled" @click="handleClick">{{
+    titleLabel
+  }}</RoundButton>
 </template>
 
 <style lang="ts" scoped>
 css({
   '.form-button': {
-    padding: '32px 148px',
-    display: 'grid',
-    placeItems: 'center',
-    width: '342px',
     margin: '0 auto',
-    borderRadius: '50px',
-    background: (props) => props.disabled ? '#C9DAEA' : '{color.vue.green}',
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: '1.25rem',
-    lineHeight: '1.75rem',
-    '&:hover': {
-      background: (props) => props.disabled ? '#C9DAEA' : 'linear-gradient(0deg, rgba(255, 255, 255, 0.24), rgba(255, 255, 255, 0.24)), {color.vue.green}',
-      boxShadow: (props) => props.disabled ? 'none' : '0px 8px 24px rgba(0, 0, 0, 0.24)',
-      transition: (props) => props.disabled ? 'none' : '.2s',
-    },
   },
 })
 </style>

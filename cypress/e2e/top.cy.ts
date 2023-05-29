21e0aef1d903a6666e14cb548173196deabab7ce
toshick <toshickjazz@mac.com>
2023-05-29 21:31:56 +0900

    feat: update top.cy.ts

--- cypress/e2e/top.cy.ts
+++ cypress/e2e/top.cy.ts
@@ -8,6 +8,7 @@ describe('top', () => {
         cy.contains('a', 'Sponsors').should('be.visible')
         cy.contains('a', 'Contact').should('be.visible')
         cy.get('a[aria-label="twitter"]').should('be.visible')
+        cy.get('.hamburger-menu').should('not.be.visible')
       })
     })
     it('header (tablet)', () => {
@@ -19,6 +20,7 @@ describe('top', () => {
         cy.contains('a', 'Sponsors').should('not.be.visible')
         cy.contains('a', 'Contact').should('not.be.visible')
         cy.get('a[aria-label="twitter"]').should('be.visible')
+        cy.get('.hamburger-menu').should('be.visible')
       })
     })
     it('header (mobile)', () => {
@@ -30,6 +32,7 @@ describe('top', () => {
         cy.contains('a', 'Sponsors').should('not.be.visible')
         cy.contains('a', 'Contact').should('not.be.visible')
         cy.get('a[aria-label="twitter"]').should('be.visible')
+        cy.get('.hamburger-menu').should('be.visible')
       })
     })
   })
@@ -38,11 +41,16 @@ describe('top', () => {
       cy.visit('/')
     })
     it('main visual', () => {
-      cy.contains('最新情報はTwitterでCheck!')
+      cy.contains('a', '最新情報はTwitterでCheck!').should(
+        'have.attr',
+        'href',
+        'https://twitter.com/vuefes',
+      )
       cy.contains('Twitter ー @vuefes #vuefes')
     })
     it('message', () => {
       cy.contains('h2', 'Message')
+        .contains('想い')
         .closest('section')
         .within(() => {
           cy.contains('Vue Fes Japan は 2018 年に誕生した日本最大級の Vue.js カンファレンスです。')
@@ -50,6 +58,7 @@ describe('top', () => {
     })
     it('sponsors', () => {
       cy.contains('h2', 'Sponsors')
+        .contains('スポンサー')
         .closest('section')
         .within(() => {
           cy.contains(
@@ -57,14 +66,19 @@ describe('top', () => {
           )
           cy.contains('a', '申し込みフォーム')
           cy.contains('a', 'スポンサー資料')
-          cy.contains(/詳細情報は、Vue Fes Japan の Twitter をご確認ください/)
+          cy.contains('a', 'Vue Fes Japan の Twitter').should(
+            'have.attr',
+            'href',
+            'https://twitter.com/vuefes',
+          )
         })
     })
   })
-  describe.only('form', () => {
+  describe('form', () => {
     it('contact', () => {
       cy.visit('/')
       cy.contains('h2', 'Contact')
+        .contains('お問い合わせ')
         .closest('section')
         .as('section')
         .within(() => {
@@ -79,15 +93,78 @@ describe('top', () => {
           cy.contains('label', 'お問い合わせ内容／Content').find('textarea').as('input-text')
         })
 
-      // validation
+      // check validation errors
 
+      cy.get('@input-name').type('a')
+      cy.wait(500)
+      cy.get('@input-name').type('{backspace}').blur()
+      cy.contains('名前を入力してください')
+      cy.get('@input-mail').type('a').type('{backspace}').blur()
+      cy.wait(500)
+      cy.contains('メールアドレスを入力してください')
+      cy.get('@input-text').type('t').type('{backspace}').blur()
+      cy.wait(500)
+      cy.contains('問い合わせ内容を入力してください')
+      cy.get('@btn-submit').should('have.attr', 'disabled')
+
+      // check activated
+
+      cy.wait(500)
       cy.get('@input-name').type('やまだ')
       cy.get('@input-mail').type('mymail@vue.com')
-      cy.get('@input-text').type('こめんと')
+      cy.get('@input-text').type('こめんと').blur()
+      cy.wait(500)
       cy.get('@btn-submit').should('not.have.attr', 'disabled')
     })
-    // it('validation', () => {
-    //   cy.get('@input-mail').type('やまだ').type('{selectAll}{backspace}')
-    // })
+  })
+  describe('footer', () => {
+    it('footer', () => {
+      cy.visit('/')
+      cy.get('.footer-section').within(() => {
+        cy.get('.footer-vuefes-logo')
+        cy.contains('a', 'Vue Fes Japan Online 2022').should(
+          'have.attr',
+          'href',
+          'https://vuefes.jp/2022',
+        )
+        cy.get('a[title="Twitter"]')
+        cy.get('a[title="note"]')
+        cy.get('a[title="YouTube"]')
+        cy.get('a[title="GitHub"]')
+        cy.contains('a', 'Vue Fes Japan 2020').should('have.attr', 'href', 'https://vuefes.jp/2020')
+        cy.contains('a', 'Vue Fes Japan 2019').should('have.attr', 'href', 'https://vuefes.jp/2019')
+        cy.contains('a', 'Vue Fes Japan 2018').should('have.attr', 'href', 'https://vuefes.jp/2018')
+        cy.contains('a', 'プライバシーポリシー').should('have.attr', 'href', '/privacy')
+        cy.contains('a', '行動規範').should('have.attr', 'href', '/code-of-conduct')
+        cy.contains('© 2018-2023 Vue.js Japan User Group some rights reserved.')
+        cy.contains(
+          'Vue.js artworks by Evan You is licensed under a Creative Commons Attribution 4.0 International License.',
+        )
+      })
+    })
+  })
+  describe('menu view', () => {
+    it('render', () => {
+      cy.viewport(769, 600)
+      cy.visit('/')
+      cy.wait(1000)
+      cy.get('.hamburger-menu').click()
+      cy.get('.mobile-menu')
+        .should('be.visible')
+        .within(() => {
+          cy.contains('a', 'Message').should('have.attr', 'href', '/#message')
+          cy.contains('a', 'Sponsors').should('have.attr', 'href', '/#sponsors')
+          cy.contains('a', 'Contact').should('have.attr', 'href', '/#form')
+          cy.contains('Vue Fes Japan')
+          cy.contains('a', '2022').should('have.attr', 'href', 'https://vuefes.jp/2022')
+          cy.contains('a', '2020').should('have.attr', 'href', 'https://vuefes.jp/2020')
+          cy.contains('a', '2019').should('have.attr', 'href', 'https://vuefes.jp/2019')
+          cy.contains('a', '2018').should('have.attr', 'href', 'https://vuefes.jp/2018')
+          cy.contains('a', 'プライバシーポリシー').should('have.attr', 'href', '/privacy')
+          cy.contains('a', '行動規範').should('have.attr', 'href', '/code-of-conduct')
+          cy.get('button.close').click()
+        })
+      cy.get('.mobile-menu').should('not.exist')
+    })
   })
 })

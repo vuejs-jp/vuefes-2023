8166cf8ca437845d3af91c19d8806b81c55cecbb
toshick <toshickjazz@mac.com>
2023-05-29 23:08:34 +0900

    fix: wait 1000

--- cypress/e2e/top.cy.ts
+++ cypress/e2e/top.cy.ts
@@ -96,24 +96,24 @@ describe('top', () => {
       // check validation errors
 
       cy.get('@input-name').type('a')
-      cy.wait(700)
+      cy.wait(1000)
       cy.get('@input-name').type('{backspace}').blur()
       cy.contains('名前を入力してください')
       cy.get('@input-mail').type('a').type('{backspace}').blur()
-      cy.wait(700)
+      cy.wait(1000)
       cy.contains('メールアドレスを入力してください')
       cy.get('@input-text').type('t').type('{backspace}').blur()
-      cy.wait(700)
+      cy.wait(1000)
       cy.contains('問い合わせ内容を入力してください')
       cy.get('@btn-submit').should('have.attr', 'disabled')
 
       // check activated
 
-      cy.wait(700)
+      cy.wait(1000)
       cy.get('@input-name').type('やまだ')
       cy.get('@input-mail').type('mymail@vue.com')
       cy.get('@input-text').type('こめんと').blur()
-      cy.wait(700)
+      cy.wait(1000)
       cy.get('@btn-submit').should('not.have.attr', 'disabled')
     })
   })
@@ -147,8 +147,8 @@ describe('top', () => {
     it('render', () => {
       cy.viewport(769, 600)
       cy.visit('/')
-      cy.wait(1000)
-      cy.get('.hamburger-menu').should('be.visible').click({ force: true })
+      cy.wait(2000)
+      cy.get('.hamburger-menu').should('be.visible').click()
       cy.get('.mobile-menu')
         .should('be.visible', { timeout: 3000 })
         .within(() => {

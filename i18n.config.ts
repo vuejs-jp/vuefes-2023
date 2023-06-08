72720a0da74b2db0f852c0183b692b1a1c45b61e
toshick <toshickjazz@mac.com>
2023-06-08 20:54:05 +0900

    feat: add top.json

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,19 +1,27 @@
 import { defineI18nConfig } from '#imports'
 import top from '~~/locales/top.json'
 
+console.log('top', top.ja)
+
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'ja',
   messages: {
-    en: {
-      welcome: 'Welcome',
-      top_twitter_update: 'さいしんじょうほう',
-      top: top.en,
-    },
     ja: {
-      welcome: 'うえるかむ',
-      top_twitter_update: 'さいしんじょうほう',
+      words: {
+        submit: '送信',
+        application_form: '申し込みフォーム',
+        sponsordoc: 'スポンサー資料',
+      },
       top: top.ja,
+    },
+    en: {
+      words: {
+        submit: 'submit',
+        application_form: 'Application form',
+        sponsordoc: '',
+      },
+      top: top.en,
     },
   },
 }))

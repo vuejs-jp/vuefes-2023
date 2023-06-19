be2556e8ad2d640727158a567df838b9d3f01873
toshick <toshickjazz@mac.com>
2023-06-20 00:12:11 +0900

    fix: toggle menu from plivacy policy

--- app/components/footer/PrivacyPolicyAndCoc.vue
+++ app/components/footer/PrivacyPolicyAndCoc.vue
@@ -4,8 +4,10 @@ import TextButton from '~/components/forms/TextButton.vue'
 
 <template>
   <div class="doc-root">
-    <TextButton url="/privacy" @click="$emit('click')">{{ $t('words.privacypolicy') }}</TextButton>
-    <TextButton url="/code-of-conduct" @click="$emit('click')">{{
+    <TextButton url="/privacy" @click.stop="$emit('click')">{{
+      $t('words.privacypolicy')
+    }}</TextButton>
+    <TextButton url="/code-of-conduct" @click.stop="$emit('click')">{{
       $t('words.codeofconduct')
     }}</TextButton>
   </div>

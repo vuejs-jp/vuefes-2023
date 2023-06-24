809bbb7b37f30bcad4ffbc5df44169abe55e59c0
nakamoto-yuto <>
2023-06-24 17:55:11 +0900

    sponsorの静的データ追加

---
+++ app/utils/sponsor.constants.ts
@@ -0,0 +1,131 @@
+export const platinumSponsors = [
+  {
+    name: 'クラウドサイン（弁護士ドットコム株式会社）',
+    image: '/sponsors/cloudsign.png',
+    category: 'platinum',
+  },
+  {
+    name: 'ユニークビジョン株式会社',
+    image: '/sponsors/unique-vision.png',
+    category: 'platinum',
+  },
+  {
+    name: '株式会社リンクアンドモチベーション',
+    image: '/sponsors/link-and-motivation-group.png',
+    category: 'platinum',
+  },
+]
+
+export const goldSponsors = [
+  {
+    name: '株式会社ヤプリ',
+    image: '/sponsors/yappli.png',
+    category: 'gold',
+  },
+  {
+    name: 'フューチャーアーキテクト株式会社',
+    image: '/sponsors/feature-architect.png',
+    category: 'gold',
+  },
+  {
+    name: 'JetBrains s.r.o.',
+    image: '/sponsors/jetbrains.png',
+    category: 'gold',
+  },
+  {
+    name: 'メドピア株式会社',
+    image: '/sponsors/medpeer.png',
+    category: 'gold',
+  },
+  {
+    name: '株式会社hacomono',
+    image: '/sponsors/hacomono.png',
+    category: 'gold',
+  },
+  {
+    name: '株式会社PLAID',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'gold',
+  },
+]
+
+export const silverSponsors = [
+  {
+    name: 'BASE株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'サイボウズ株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'MNTSQ株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: '株式会社MIERUNE',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: '株式会社ビザスク',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'STORES 株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'LINE Fukuoka株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'ラクスル株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: 'LAPRAS株式会社',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+  {
+    name: '株式会社ギフティ',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'silver',
+  },
+]
+
+export const bronzeSponsors = [
+  {
+    name: '株式会社タノム',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'bronze',
+  },
+  {
+    name: '株式会社スタディスト',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'bronze',
+  },
+  {
+    name: '株式会社coco',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'bronze',
+  },
+  {
+    name: '株式会社TENTIAL',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'bronze',
+  },
+  {
+    name: '株式会社メルペイ',
+    image: '/sponsors/hacomono.png', //TODO: 画像変更
+    category: 'bronze',
+  },
+]

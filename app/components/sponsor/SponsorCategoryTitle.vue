b653c61f6fe05c8d6ca8f231736fb7c140f34071
nakamoto-yuto <>
2023-06-24 17:56:10 +0900

    add SponsorCategoryTitle component

---
+++ app/components/sponsor/SponsorCategoryTitle.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { SponsorCategory } from '~/types/app'
+
+const props = defineProps({
+  category: {
+    type: String as PropType<SponsorCategory>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <h3 class="sponsor-category-title">
+    {{ category }}
+  </h3>
+</template>
+
+<style lang="ts" scoped>
+css({
+  '.sponsor-category-title': {
+    width: '100%',
+    color: (props) => `{color.sponsor.${props.category}}`,
+    borderBottom: (props) => `5px solid {color.sponsor.${props.category}}`,
+    fontWeight: 'bold',
+    textTransform: 'capitalize'
+  },
+})
+</style>

cc989d8b0d6700e6585f460d63a9fc3c0c1a0901
aster-mnch <mnch.mai@gmail.com>
2023-08-01 03:36:56 +0900

    feat: Add access information to the venue
    Photos were obtained from the following site.
    
    https://www.nakano-centralpark.jp/conference/hall

--- app/components/venue/AccessSection.vue
+++ app/components/venue/AccessSection.vue
@@ -12,7 +12,41 @@ import RoundButton from '~/components/button/RoundButton.vue'
         :yamato-title="$t('top.access_subtitle')"
       />
       <div class="access-content">
-        <!-- TODO -->
+        <div class="images">
+          <img
+            class="image image-hall"
+            alt="イベントホールの写真"
+            src="/venue/hall.jpeg"
+            decoding="async"
+          />
+          <img
+            class="image image-entrance"
+            alt="イベント入り口の写真"
+            src="/venue/entrance.jpg"
+            decoding="async"
+          />
+        </div>
+        <div class="route">
+          <h3>会場: 中野セントラルパーク カンファレンス</h3>
+          <p>
+            東京都中野区中野 4-10-2 中野セントラルパークサウス 1F、B1F
+            <br />
+            <a
+              href="https://www.nakano-centralpark.jp/conference/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://www.nakano-centralpark.jp/conference/
+            </a>
+          </p>
+          <p>
+            JR 中央線・総武線、東京メトロ東西線
+            <br />
+            「中野」駅北口より徒歩5分
+            <br />
+            <small>※JR新宿駅から中野駅まで中央線で1駅（約4分）です。</small>
+          </p>
+        </div>
       </div>
       <RoundButton href="https://goo.gl/maps/TBWRVrd3dthoe98s5" target="_blank">
         Google マップで見る
@@ -43,6 +77,35 @@ css({
   '.access-content': {
     display: 'flex',
     gap: 'calc({space.8} * 3)',
+  },
+  '.images': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 'calc({space.8} * 1.5)',
+
+    '.image': {
+      flexBasis: '100%',
+      aspectRatio: '3/2',
+      objectFit: 'cover',
+    },
+  },
+  '.route': {
+    flexGrow: '1',
+
+    'h3': {
+      fontWeight: 700,
+      fontSize: 'calc(24*{fontSize.base})',
+    },
+    'p': {
+      fontSize: 'calc(18*{fontSize.base})',
+      marginTop: 'calc({space.8} * 3)',
+    },
+    'small': {
+      fontSize: 'calc(12*{fontSize.base})',
+    },
+    'a': {
+      textDecoration: 'underline',
+    },
   },
 })
 </style>

92174f2fe0d4e78bc4d44bc85628a6631566e950
jiyuujin <jiyuujinunite@gmail.com>
2023-08-15 13:01:09 +0900

    fix: ci

--- tests/cypress/e2e/sponsors-detail.cy.ts
+++ tests/cypress/e2e/sponsors-detail.cy.ts
@@ -12,7 +12,8 @@ describe('sponsors detail', () => {
     it('cloudsign', () => {
       loadPage('cloudsign', 'platinum')
       cy.contains('h2', 'スポンサー')
-      cy.contains('h1', 'クラウドサイン（弁護士ドットコム株式会社）')
+      cy.wait(500)
+      cy.contains('h2', 'クラウドサイン（弁護士ドットコム株式会社）')
       cy.get('img[alt="クラウドサイン（弁護士ドットコム株式会社）のロゴ"]')
     })
   })
@@ -20,7 +21,8 @@ describe('sponsors detail', () => {
     it('yappli', () => {
       loadPage('yappli', 'gold')
       cy.contains('h2', 'スポンサー')
-      cy.contains('h1', '株式会社ヤプリ')
+      cy.wait(500)
+      cy.contains('h2', '株式会社ヤプリ')
       cy.get('img[alt="株式会社ヤプリのロゴ"]')
     })
   })
@@ -28,7 +30,8 @@ describe('sponsors detail', () => {
     it('base', () => {
       loadPage('base', 'silver')
       cy.contains('h2', 'スポンサー')
-      cy.contains('h1', 'BASE株式会社')
+      cy.wait(500)
+      cy.contains('h2', 'BASE株式会社')
       cy.get('img[alt="BASE株式会社のロゴ"]')
     })
   })
@@ -36,7 +39,8 @@ describe('sponsors detail', () => {
     it('base', () => {
       loadPage('tanomu', 'bronze')
       cy.contains('h2', 'スポンサー')
-      cy.contains('h1', '株式会社タノム')
+      cy.wait(500)
+      cy.contains('h2', '株式会社タノム')
       cy.get('img[alt="株式会社タノムのロゴ"]')
     })
   })

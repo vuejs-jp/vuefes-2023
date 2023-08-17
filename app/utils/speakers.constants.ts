b1f65cd6e5c78116a7ed23777fdd34cbfa65af24
YUMA Kitamura <jiyuujinunite@gmail.com>
2023-08-17 14:22:24 +0900

    Update app/utils/speakers.constants.ts

--- app/utils/speakers.constants.ts
+++ app/utils/speakers.constants.ts
@@ -366,7 +366,7 @@ export const speakers: Speaker[] = [
       type: 'lt',
     },
     profile: {
-      image: '/speakers/fuqda.jpg',
+      image: '/speakers/fuqda.jpeg',
       title: '',
       name: 'fuqda / フクダ',
       twitterId: 'fuqda90',

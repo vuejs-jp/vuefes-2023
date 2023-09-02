84b6d74bc827b0ddfe052342bfdb4ed82df6d59e
jiyuujin <jiyuujinunite@gmail.com>
2023-09-02 22:28:35 +0900

    sessions detail tests

---
+++ tests/cypress/e2e/sessions-detail.cy.ts
@@ -0,0 +1,65 @@
+describe('sessions detail', () => {
+  function loadPage(speakerId: string) {
+    cy.intercept('/api/_supabase/session')
+    cy.visit('/')
+    cy.checkPageIdle()
+    cy.wait(500)
+    cy.get(`.speakers a[href="/sessions/${speakerId}"]`).click({ force: true })
+    cy.url().should('eq', `http://localhost:3000/sessions/${speakerId}`)
+  }
+
+  describe('yyx990803 sessions', () => {
+    it('yyx990803', () => {
+      loadPage('yyx990803')
+      cy.contains('h2', 'キーノート')
+      cy.wait(500)
+      cy.contains('p', 'Evan You')
+      cy.get('img[alt="Evan Youの写真"]')
+    })
+  })
+  describe('atinux sessions', () => {
+    it('atinux', () => {
+      loadPage('atinux')
+      cy.contains('h2', 'Nuxt to the Edge')
+      cy.wait(500)
+      cy.contains('p', 'Sebastien Chopin')
+      cy.get('img[alt="Sebastien Chopinの写真"]')
+    })
+  })
+  describe('patak-dev sessions', () => {
+    it('patak-dev', () => {
+      loadPage('patak-dev')
+      cy.contains('h2', 'セッション')
+      cy.wait(500)
+      cy.contains('p', 'Matias Capeletto')
+      cy.get('img[alt="Matias Capelettoの写真"]')
+    })
+  })
+  describe('antfu sessions', () => {
+    it('antfu', () => {
+      loadPage('antfu')
+      cy.contains('h2', 'セッション')
+      cy.wait(500)
+      cy.contains('p', 'Anthony Fu')
+      cy.get('img[alt="Anthony Fuの写真"]')
+    })
+  })
+  describe('daniel-roe sessions', () => {
+    it('daniel-roe', () => {
+      loadPage('daniel-roe')
+      cy.contains('h2', 'セッション')
+      cy.wait(500)
+      cy.contains('p', 'Daniel Roe')
+      cy.get('img[alt="Daniel Roeの写真"]')
+    })
+  })
+  describe('daniel-roe sessions', () => {
+    it('daniel-roe', () => {
+      loadPage('daniel-roe')
+      cy.contains('h2', 'セッション')
+      cy.wait(500)
+      cy.contains('p', 'Daniel Roe')
+      cy.get('img[alt="Daniel Roeの写真"]')
+    })
+  })
+})

30085d154bfa44aa0fc10987c8a6ee99845df8fc
jiyuujin <jiyuujinunite@gmail.com>
2023-09-04 19:43:27 +0900

    Avatar

--- app/composables/useSupabase.ts
+++ app/composables/useSupabase.ts
@@ -44,6 +44,8 @@ export function useSupabase() {
   }
 
   function getFullAvatarUrl(avatarUrl: string) {
+    if (avatarUrl.startsWith('https://') || avatarUrl.startsWith(supabaseProjectUrl))
+      return avatarUrl
     return `${supabaseProjectUrl}/storage/v1/object/public/avatar/${avatarUrl}`
   }
 

d5bccf4ee9deac37dc46dd7e379ced504dc214db
jiyuujin <jiyuujinunite@gmail.com>
2023-09-13 14:23:40 +0900

    fix: image path

--- app/utils/events-vuejs-clinic.constants.ts
+++ app/utils/events-vuejs-clinic.constants.ts
@@ -2,21 +2,21 @@ import { Paneler } from '~/types/app'
 
 export const experts: Paneler[] = [
   {
-    image: '/speakers/evan.jpg',
+    image: 'speakers/evan.jpg',
     name: 'Evan You',
     snsLink: 'https://twitter.com/youyuxi',
     company: '',
     job: 'Vue / Viteクリエーター',
   },
   {
-    image: '/speakers/sebastien.jpg',
+    image: 'speakers/sebastien.jpg',
     name: 'Sebastien Chopin',
     snsLink: 'https://twitter.com/Atinux/',
     company: 'NuxtLabs CEO',
     job: '',
   },
   {
-    image: '/speakers/daniel_roe.jpg',
+    image: 'speakers/daniel_roe.jpg',
     name: 'Daniel Roe',
     snsLink: 'https://twitter.com/danielcroe',
     company: '',

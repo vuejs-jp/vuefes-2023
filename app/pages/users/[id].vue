e5537624c0243ef356982282af0b933d9ad1e807
jiyuujin <jiyuujinunite@gmail.com>
2023-09-17 15:51:38 +0900

    fix: avatar - #517

--- app/pages/users/[id].vue
+++ app/pages/users/[id].vue
@@ -74,8 +74,8 @@ useHead({
       <AvatarCard
         :signed-user="{
           ...signedUser,
-          full_name: eventUser?.display_name || eventUser?.full_name || '参加者',
-          avatar_url: eventUser?.avatar_url || signedUser.avatar_url,
+          full_name: eventUser?.display_name || '参加者',
+          avatar_url: eventUser?.avatar_url || '',
           role: eventUser?.role || 'attendee',
         }"
         :opacity="eventUser?.activated_at ? 1 : 0.6"

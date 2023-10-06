4aa4eb9c0f8296b532c356e5840b663c830b2424
jiyuujin <jiyuujinunite@gmail.com>
2023-10-06 13:58:53 +0900

    fix: ci

--- tests/cypress/e2e/top.cy.ts
+++ tests/cypress/e2e/top.cy.ts
@@ -161,7 +161,7 @@ describe('top', () => {
         .closest('section')
         .as('section')
         .within(() => {
-          cy.contains('button', '送信').as('btn-submit').should('have.attr', 'disabled')
+          cy.contains('button', '送信').as('btn-submit').should('have.class', '-disabled')
           cy.contains('Vue Fes Japan にご興味をいただき、ありがとうございます')
           cy.contains('label', 'お名前／Name')
             .find('input[placeholder="山田太郎"]')

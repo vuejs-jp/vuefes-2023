77d9536d6a750c9a0b81151ea23d392694fd8060
jiyuujin <jiyuujinunite@gmail.com>
2023-10-20 09:28:40 +0900

    closedPreOrder

--- app/utils/status.constants.ts
+++ app/utils/status.constants.ts
@@ -4,4 +4,4 @@ export const closedVolunteer = true
 export const closedHandson = true
 export const closedEarlyPrice = true
 export const closedPrice = true
-export const closedPreOrder = false
+export const closedPreOrder = true
